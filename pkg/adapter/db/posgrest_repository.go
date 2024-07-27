071f23c8077b0ea3dbf83d550f1f0bbe4208157b
J4F3ET <jslopezz@udistrital.edu.co>
2024-07-27 18:33:46 -0500

    feat: :sparkles: New feacture implement TaskPosgrestRepository

--- pkg/adapter/db/posgrest_repository.go
+++ pkg/adapter/db/posgrest_repository.go
@@ -1,1 +1,44 @@
 package db
+
+import (
+	"todo-app/internal/domain/entity"
+	"todo-app/internal/repository"
+)
+
+type TaskPostgresRepository struct {
+	conn *SingletonConnPostgres
+}
+func NewTaskPostgresRepository() (repository.TaskRepository,error) {
+	conn,err := getInstance()
+	if err != nil{
+		return nil,err
+	}
+	return &TaskPostgresRepository{conn: conn},nil
+}
+// Create implements repository.TaskRepository.
+func (t *TaskPostgresRepository) Create(task *entity.Task) (*entity.Task, error) {
+
+	panic("unimplemented")
+}
+
+// Delete implements repository.TaskRepository.
+func (t *TaskPostgresRepository) Delete(id int64) error {
+	panic("unimplemented")
+}
+
+// GetAll implements repository.TaskRepository.
+func (t *TaskPostgresRepository) GetAll() ([]*entity.Task, error) {
+	panic("unimplemented")
+}
+
+// GetById implements repository.TaskRepository.
+func (t *TaskPostgresRepository) GetById(id int64) (*entity.Task, error) {
+	panic("unimplemented")
+}
+
+// Update implements repository.TaskRepository.
+func (t *TaskPostgresRepository) Update(task *entity.Task) (*entity.Task, error) {
+	panic("unimplemented")
+}
+
+
